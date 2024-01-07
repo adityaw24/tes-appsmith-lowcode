e9ca2a317351fe5cb70726ea9109e46f2b36ff9e
adityawisnu104 <adityawisnu104@gmail.com>
2024-01-07 15:39:38 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/dataForm/dataForm.js
@@ -0,0 +1,12 @@
+export default {
+	genderList: [
+		{
+			"label": "Male",
+			"value": "male"
+		},
+		{
+			"label": "Female",
+			"value": "female"
+		}
+	]
+}
---
+++ pages/Page1/jsobjects/submitFunc/submitFunc.js
@@ -0,0 +1,3 @@
+export default {
+	error: (error) => showAlert(`Error ${error}`,'error')
+}
